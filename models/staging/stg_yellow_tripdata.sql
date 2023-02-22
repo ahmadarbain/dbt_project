412c1479ed0a23841c8b9a2918f0c18a8f183859
ahmad.arbain1014@students.unila.ac.id <ahmad.arbain1014@students.unila.ac.id>
2023-02-22 17:11:34 +0000

    last commit

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -10,7 +10,6 @@ with
         from {{ source("staging", "yellow_tripdata") }}
         where vendorid is not null
     )
-
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(["vendorid", "tpep_pickup_datetime"]) }} as tripid,
@@ -42,8 +41,8 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description("payment_type") }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
-
 from tripdata
 where rn = 1
+
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var("is_test_run", default=true) %} limit 100 {% endif %}
